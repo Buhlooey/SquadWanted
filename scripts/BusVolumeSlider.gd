5bf7effa28c0bccd73106393bd8171c0fb04d9b0
Buhlooey <108116990+Buhlooey@users.noreply.github.com>
2026-07-30 12:58:50 -0700

    Update volume sliders

--- scripts/BusVolumeSlider.gd
+++ scripts/BusVolumeSlider.gd
@@ -3,9 +3,14 @@ extends VSlider
 @export var busName: String
 @onready var bus: FmodBus = FmodServer.get_bus("bus:/" + busName)
 
+@onready var nameText: RichTextLabel = $SliderName
+@onready var valueText: RichTextLabel = $SliderValue
+
 func _ready():
 	# FmodServer.wait_for_all_loads()
+	nameText.text = busName
 	value = bus.get_volume()
 
 func _on_value_changed(newValue: float) -> void:
 	bus.set_volume(newValue)
+	valueText.text = str(int(newValue * 100), "%")
